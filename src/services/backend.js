326cb4b928387474db5dee226cf47bf07147bad8
Endy <endy@easywork.asia>
2021-02-08 03:19:37 +0800

    update gitignore

--- src/services/backend.js
+++
@@ -1,9 +0,0 @@
-const apiUrl = process.env.REACT_APP_API_URL
-
-const get = async function (url) {
-  let res = await fetch(`${apiUrl}/${url}`)
-  res = await res.json()
-  return res
-}
-
-export { get }
